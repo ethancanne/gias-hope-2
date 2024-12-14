c54f0bb5bd605e6fb2b0a425a77d6ac6e86927c0
Ethan Cannelongo <ethancannelongo@gmail.com>
2024-12-13 22:40:45 -0500

    init commit

---
+++ src/views/home/4-our-vision/ourVision.view.tsx
@@ -0,0 +1,6 @@
+import styles from './ourVision.module.scss';
+type Props = {};
+const OurVision = (props: Props) => {
+  return <div className={styles.container}>OurVisionComponent</div>;
+};
+export default OurVision;
