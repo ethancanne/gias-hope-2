b576eadcf3d98c3f01571fad4411a04e9d2d6f16
Ethan Cannelongo <ethancannelongo@gmail.com>
2025-01-15 15:00:48 -0500

    Made the site incrediblily more responsive, and improved the design a bit

--- src/layout/intro/intro.component.tsx
+++ src/layout/intro/intro.component.tsx
@@ -12,57 +12,59 @@ type Props = {
 
 const Intro = (props: Props) => {
   return (
-    <div className={styles.wrapper}>
-      <div
-        className={
-          styles.container +
-          ' ' +
-          styles[props.size] +
-          ' ' +
-          (props.mobileBackgroundImg ? '' : styles.noBackground)
-        }
-      >
-        {props.desktopBackgroundImg && (
-          <Image
-            src={props.desktopBackgroundImg}
-            alt="background"
-            width={1920}
-            height={680}
-            className={styles.largeBackgroundImg}
-          />
-        )}
-        {props.mobileBackgroundImg && (
-          <Image
-            src={props.mobileBackgroundImg}
-            alt="background"
-            width={1920}
-            height={680}
-            className={styles.smallBackgroundImg}
-          />
-        )}
-        {props.size === 'large' && (
-          <Image
-            src={'/tree.png'}
-            width={600}
-            height={500}
-            alt="tree"
-            className={styles.tree}
-          />
-        )}
-
-        <div className={styles.content}>
-          {props.headerImg ? (
+    <div className={styles.shadowWrapper}>
+      <div className={styles.wrapper}>
+        <div
+          className={
+            styles.container +
+            ' ' +
+            styles[props.size] +
+            ' ' +
+            (props.mobileBackgroundImg ? '' : styles.noBackground)
+          }
+        >
+          {props.desktopBackgroundImg && (
             <Image
-              src={props.headerImg}
-              width={500}
-              height={80}
-              alt="logo"
-              className={styles.headerImg}
+              src={props.desktopBackgroundImg}
+              alt="background"
+              width={1920}
+              height={680}
+              className={styles.largeBackgroundImg}
             />
-          ) : (
-            <h1 className={styles.headerTxt}>{props.title || 'Header'}</h1>
           )}
-          <p className={styles.subText}>{props.description || 'Subtext'}</p>
+          {props.mobileBackgroundImg && (
+            <Image
+              src={props.mobileBackgroundImg}
+              alt="background"
+              width={1920}
+              height={680}
+              className={styles.smallBackgroundImg}
+            />
+          )}
+          {props.size === 'large' && (
+            <Image
+              src={'/tree.png'}
+              width={600}
+              height={500}
+              alt="tree"
+              className={styles.tree}
+            />
+          )}
+
+          <div className={styles.content}>
+            {props.headerImg ? (
+              <Image
+                src={props.headerImg}
+                width={500}
+                height={80}
+                alt="logo"
+                className={styles.headerImg}
+              />
+            ) : (
+              <h1 className={styles.headerTxt}>{props.title || 'Header'}</h1>
+            )}
+            <p className={styles.subText}>{props.description || 'Subtext'}</p>
+          </div>
         </div>
       </div>
     </div>
--- src/views/our-impact/2-impact-details/impact-details.view.tsx
+++ src/views/our-impact/2-impact-details/impact-details.view.tsx
@@ -17,38 +17,40 @@ const ImpactDetails = (props: Props) => {
   return (
     <div className={styles.container}>
       <div className={styles.detailsContainer}>
-        {props.impactDetailsData.map((data, index) => (
-          <div key={index} className={styles.detail}>
-            <Image
-              src={data.image}
-              alt={data.title}
-              width={500}
-              height={300}
-              className={styles.image}
-            />
+        <div className={styles.detailsWrapper}>
+          {props.impactDetailsData.map((data, index) => (
+            <div key={index} className={styles.detail}>
+              <Image
+                src={data.image}
+                alt={data.title}
+                width={500}
+                height={300}
+                className={styles.image}
+              />
 
-            <div className={styles.textContainer}>
-              <Title small className={styles.title}>
-                {data.title}
-              </Title>
-              <Paragraph small className={styles.description}>
-                {data.description}
-              </Paragraph>
-              {data.link && (
-                <Button
-                  href={data.link}
-                  type="yellow"
-                  className={styles.button}
-                >
-                  Learn More
-                </Button>
-              )}
+              <div className={styles.textContainer}>
+                <Title small className={styles.title}>
+                  {data.title}
+                </Title>
+                <Paragraph small className={styles.description}>
+                  {data.description}
+                </Paragraph>
+                {data.link && (
+                  <Button
+                    href={data.link}
+                    type="yellow"
+                    className={styles.button}
+                  >
+                    Learn More
+                  </Button>
+                )}
+              </div>
+
+              <img className={styles.line} src={'/line.svg'} alt={'line'} />
+              <img className={styles.line2} src={'/line-2.svg'} alt={'line'} />
             </div>
-
-            <img className={styles.line} src={'/line.svg'} alt={'line'} />
-            <img className={styles.line2} src={'/line-2.svg'} alt={'line'} />
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
