b1034d89babb1595b3edf92aae38534a9fdb8a02
Ethan Cannelongo <ethancannelongo@gmail.com>
2025-01-15 15:12:07 -0500

    Fixed build error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="html">
       <body
         className={`${serifFont.variable}  ${sansFont.variable}  ${serifBoldFont.variable} body`}
       >
