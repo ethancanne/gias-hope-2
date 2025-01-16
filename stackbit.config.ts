ca3d212b2e48a511e5bd858c4f4ab4bc444be53c
Ethan Cannelongo <ethancannelongo@gmail.com>
2025-01-16 15:54:59 -0500

    Removed pattern from stackbit config

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -285,7 +285,6 @@ export default defineStackbitConfig({
                   {
                     name: 'name',
                     type: 'string',
-                    pattern: '^[a-z0-9]+(?:-[a-z0-9]+)*$',
                   },
                   {
                     name: 'type',
