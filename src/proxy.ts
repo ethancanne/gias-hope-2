4929a21a2f37c5eabf9cdcb0d54e7282378c56d4
Ethan Cannelongo <ethancannelongo@gmail.com>
2026-01-10 09:09:17 -0500

    Upgrade NextJS

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function middleware(req: NextRequest) {
+export async function proxy(req: NextRequest) {
   const requestHeaders = new Headers(req.headers);
   requestHeaders.set('x-request-url', req.url);
 
